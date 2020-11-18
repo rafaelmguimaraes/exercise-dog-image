4f80991fda31c23009a0904e28ece132129577e1
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-11-17 22:29:00 -0300

    Solving Exercises 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    console.log('constructor')
+    super(props);
+    //this.getNextDog = this.getNextDog(this);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      dogImage: '',
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.getNextDog();
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+    console.log(nextState.dogImage);
+    if (nextState.dogImage.includes('terrier'))
+      return false;
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+    const { dogImage } = this.state;
+    if (dogImage) {
+      localStorage.setItem("lastDogImage", JSON.stringify(dogImage));
+      const regexBreed = /breeds\/(.*)\//gm;
+      const breed = regexBreed.exec(dogImage)[1];
+      alert(breed);
+    }
+  }
+  
+
+  getNextDog = () => {
+    this.setState({ isLoaded: false }, () => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              dogImage: result.message
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    })
+  }
+
+  render() {
+    console.log('render')
+    const { error, isLoaded, dogImage } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+
+      return (
+        <div className="main">
+          <img src={dogImage} alt="Random Dog"/>
+          <button onClick={this.getNextDog} >Next Dog</button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
